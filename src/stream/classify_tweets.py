54744069edf62f3cb132177be72c31860303ab34
jackma-00 <jacopo.maragna1@gmail.com>
2024-10-05 10:38:50 +0200

    Refactor classify_tweets.py and update sentiment analysis
    This commit refactors the classify_tweets.py file and updates the sentiment analysis functionality. The SUPPORT_KEY and OPPOSE_KEY variables have been changed to Positive and Negative respectively. Additionally, a new NEUTRAL_KEY variable has been added. The code now connects to MongoDB and initializes a Spark session. The SentimentAnalyzer object is broadcasted to all worker nodes for classification of tweets. The perform_inference function is defined to classify the tweets using the SentimentAnalyzer. The classify_trump and classify_kamala functions now use the perform_inference function for model evaluation. The handle_stream function has been removed from the spark session initialization.

--- src/stream/classify_tweets.py
+++ src/stream/classify_tweets.py
@@ -6,11 +6,16 @@ import logging
 from pymongo import MongoClient
 from datetime import datetime
 
+from sentiment_analysis.politics import SentimentAnalyzer
+
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-SUPPORT_KEY = "support"
-OPPOSE_KEY = "oppose"
+
+SUPPORT_KEY = "Positive"
+OPPOSE_KEY = "Negative"
+NEUTRAL_KEY = "Neutral"
 
 T_SUPPORT_COUNT = 0
 T_OPPOSE_COUNT = 0
@@ -18,11 +23,31 @@ T_OPPOSE_COUNT = 0
 K_SUPPORT_COUNT = 0
 K_OPPOSE_COUNT = 0
 
+# Connect to MongoDB
 client = MongoClient("mongodb://localhost:27017/")
 db = client["usa2024"]  # use or create a database named usa2024
 trump_db = db["trump"]  # use or create a collection named trump
 harris_db = db["harris"]  # use or create a collection named harris
 
+# Initialize the Spark session
+spark = SparkSession.builder \
+    .appName("KafkaSparkStreamingJSON") \
+    .config("spark.jars.packages",
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,") \
+    .getOrCreate()
+sc = spark.sparkContext
+
+# Initialize the SentimentAnalyzer
+analyzer = SentimentAnalyzer()
+
+# Broadcast the SentimentAnalyzer object to all the worker nodes
+analyzer_bc = sc.broadcast(analyzer)
+
+# Define the function to classify the tweets
+def perform_inference(tweet: str):
+    model = analyzer_bc.value
+    model_output = model.analyze_sentiment(tweet)
+    return tuple(model_output[1], 1)
 
 def classify_trump(batch_df: DataFrame, batch_id: int):
     # use map in the current stream batch
@@ -31,8 +56,7 @@ def classify_trump(batch_df: DataFrame, batch_id: int):
     classified_tweets = batch_df.rdd.map(
         # the map function evaluates the tweets and generates a key-value pair
         # where the key is the sentiment and the value is 1 to count the number of tweets
-
-        lambda row: (SUPPORT_KEY, 1))  # add model evaluation here
+        perform_inference)
     counts = classified_tweets.reduceByKey(lambda x, y: x + y)
     global T_SUPPORT_COUNT, T_OPPOSE_COUNT
 
@@ -56,8 +80,7 @@ def classify_kamala(batch_df: DataFrame, batch_id: int):
     classified_tweets = batch_df.rdd.map(
         # the map function evaluates the tweets and generates a key-value pair
         # where the key is the sentiment and the value is 1 to count the number of tweets
-
-        lambda row: (SUPPORT_KEY, 1))  # add model evaluation here
+        perform_inference)  # add model evaluation here
     counts = classified_tweets.reduceByKey(lambda x, y: x + y)
     global K_SUPPORT_COUNT, K_OPPOSE_COUNT
 
@@ -76,12 +99,6 @@ def classify_kamala(batch_df: DataFrame, batch_id: int):
 
 
 def handle_stream():
-    spark = SparkSession.builder \
-        .appName("KafkaSparkStreamingJSON") \
-        .config("spark.jars.packages",
-                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,") \
-        .getOrCreate()
-
     df = spark.readStream.format("kafka") \
         .option("kafka.bootstrap.servers",
                 "localhost:9092,localhost:9093,localhost:9094") \
